e5e0b3bbd8a9d4dfe12f6ecf7ff8fbcf2cdac33d
Henri Linkletter <henrilinkletter@gmail.com>
2023-11-15 21:31:44 -0500

    up to number 3 done

--- script-01.js
+++ script-01.js
@@ -55,15 +55,51 @@ const pokemons = [
 
 // 1. Display the Pokemons that have a level between 30 and 50
 
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].level >= 30 && pokemons[i].level <=50) {
+    console.log(pokemons[i].name);
+  }
+}
+
+console.log("Abilities");
+
+
 
 // 2. Display the pokemons that have an ability called "Run Away"
 
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].abilities.includes('Run Away')) {
+    console.log(pokemons[i].name);
+  }
+}
+
+console.log("Lightning Rod Test");
+
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].abilities.includes("Lightning Rod")) {
+    console.log(pokemons[i].name);
+  }
+}
 
 // 3. Display how many Pokemons have the type "Normal"
 
+let normalCount = 0;
+
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].type === "Normal") {
+    normalCount++;
+  }
+}
+
+console.log("There are",normalCount, "normal type Pokemon.");
 
 // 4. Display the names of all of pokemons that have a type of "Psychic"
 
+for (let i = 0; i < pokemons.length; i++) {
+  if (pokemons[i].type === "Psychic") {
+    console.log(pokemons[i].name, "is a Psychic type Pokemon.")
+  }
+}
 
 // 5. Display the types of Pokemon and the number of Pokemons that have that type
 //   Example:
@@ -73,5 +109,38 @@ const pokemons = [
 // Create an array of all the types
 // eg: { type: 'Normal', count: 3 }
 
+let types = [
+  { type: 'Normal', count: 0 },
+  { type: 'Electric', count: 0 },
+  { type: 'Fire', count: 0 },
+  { type: 'Grass', count: 0 },
+  { type: 'Water', count: 0 },
+  { type: 'Ghost', count: 0 },
+  { type: 'Psychic', count: 0 },
+  { type: 'Fighting', count: 0 },
+  { type: 'Dragon', count: 0 },
+  { type: 'Rock', count: 0 },
+  { type: 'Ground', count: 0 },
+  { type: 'Ice', count: 0 },
+  { type: 'Poison', count: 0 },
+  { type: 'Bug', count: 0 },
+  { type: 'Flying', count: 0 },
+  { type: 'Dark', count: 0 },
+  { type: 'Steel', count: 0 },
+  { type: 'Fairy', count: 0 },
+];
+
+for (i = 0; i < pokemons.length; i++) {
+  for (j = 0; j < types.length; j++) {
+    if (pokemons[i].type === types[j].type) {
+      types[j].count++;
+      break;
+    }
+  }
+}
 
 // Display the results
+
+for (i = 0; i < types.length; i++) {
+  console.log("There are", types[i].count, types[i].type, "type Pokemon.")
+}
--- script-02.js
+++ script-02.js
@@ -38,9 +38,16 @@ const pokemons = [
 // 1. Get a reference to the list already on the page
 // Recommend using querySelector to get the list
 
+const pokeList = document.querySelector(".poke-items");
 
 // 2. Loop through the array of pokemons items
 // For each item:
 //  Create an a new list item element
 //  Add text content to the list item that is from the array
 //  Add the new item to the list on the page
+
+for (let i = 0; i < pokemons.length; i++) {
+  let pokemonListEntry = document.createElement("li");
+  pokemonListEntry.textContent = pokemons[i].name;
+  pokeList.appendChild(pokemonListEntry);
+}
